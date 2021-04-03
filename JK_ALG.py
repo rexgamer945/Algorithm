e524fe3ad9eff5fec2c353c92f602f072315f739
rexgamer945 <75562022+rexgamer945@users.noreply.github.com>
2021-04-03 17:01:47 +1100

    Update JK_ALG.py

--- JK_ALG.py
+++ JK_ALG.py
@@ -13,7 +13,7 @@ test_y = y[80:]
 
 mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
 
-myline = numpy.linspace(0, 6, 100)
+myline = numpy.linspace(0, 6, 100, 1000)
 
 plt.scatter(train_x, train_y)
 plt.plot(myline, mymodel(myline))
